fc72854766a3a4fcef11ae18713a756a8e621fb0
davidjnunez <david.nunez@rightscale.com>
2012-03-07 00:55:12 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/Chef/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: Chef
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello World! You should try Chef with RightScale!"

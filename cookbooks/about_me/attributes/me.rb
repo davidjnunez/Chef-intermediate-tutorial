9743fb8b1a2c4372ea81d0e2f6152ee5088e45b3
davidjnunez <david.nunez@rightscale.com>
2012-04-16 16:06:57 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/attributes/me.rb
@@ -0,0 +1,4 @@
+set[:about_me][:first_name] = "David"
+set_unless[:about_me][:favorite][:food] = "pizza"
+set_unless[:about_me][:favorite][:drink] = "water"
+set_unless[:about_me][:favorite][:hobby] = "running"
